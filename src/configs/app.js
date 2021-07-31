e5f9d2985a598c5ce038a0b078707b74c3242235
ThomasDos <dosanjos.thomas@gmail.com>
2021-07-31 12:08:06 +0200

    update / test heroku

--- src/configs/app.js
+++ src/configs/app.js
@@ -11,10 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../../client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
   });
 }
 
