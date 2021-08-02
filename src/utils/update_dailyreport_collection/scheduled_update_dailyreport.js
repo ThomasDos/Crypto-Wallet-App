c6ed44bca2da36664866d897083e5d61e8fb19f4
ThomasDos <dosanjos.thomas@gmail.com>
2021-08-02 19:56:45 +0200

    update

--- src/utils/update_dailyreport_collection/scheduled_update_dailyreport.js
+++ src/utils/update_dailyreport_collection/scheduled_update_dailyreport.js
@@ -3,6 +3,6 @@ const updateDailyReport = require("./update_daily_report");
 
 module.exports = () =>
   cron.schedule("0 1 0 * * *", () => {
-    //! Running everyday at midnight and 1 minute
+    //! Running everyday at midnight and 1 minute.
     updateDailyReport();
   });
